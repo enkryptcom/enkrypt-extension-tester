abd1251aa6ec0b94a7d11acc38fb0327efc8fc16
Abyscuit <marcus14530@gmail.com>
2022-05-05 11:33:28 -0700

    feat: add basic actions

--- src/views/DappTester/DappTester.vue
+++ src/views/DappTester/DappTester.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="py-10">
     <v-row>
-      <v-col cols="12" md="4"><BasicActions /></v-col>
+      <v-col cols="12" md="4"><BasicActions :ethereum="ethereum"/></v-col>
       <v-col cols="12" md="4"><PermissionsActions /></v-col>
       <v-col cols="12" md="4"><SendEth /></v-col>
       <v-col cols="12" md="4"><Contract /></v-col>
@@ -36,6 +36,7 @@ import EthereumChainInteractions from './components/EthereumChainInteractions/Et
 import SendForm from './components/SendForm/SendForm.vue';
 
 import { defineComponent } from 'vue';
+import { ethers } from 'ethers';
 
 export default defineComponent({
   name: 'DappTester',
@@ -54,6 +55,17 @@ export default defineComponent({
     SignTypedDataV4,
     EthereumChainInteractions,
     SendForm
+  },
+  data(){
+    return{
+      ethersProvider: {},
+      ethereum: {}
+    }
+  },
+  mounted(){
+    // We must specify the network as 'any' for ethers to allow network changes
+    this.ethersProvider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+    this.ethereum = window.ethereum;
   }
 });
 </script>
--- src/views/DappTester/components/BasicActions/BasicActions.vue
+++ src/views/DappTester/components/BasicActions/BasicActions.vue
@@ -1,8 +1,15 @@
 <template>
   <CustomCard title="Basic Actions">
-    <CustomBtn>Connect</CustomBtn>
-    <CustomBtn>eth_accounts</CustomBtn>
-    <CustomTextbox title="eth_accounts result">null</CustomTextbox>
+    <CustomBtn 
+      :disabled="isDisabled"
+      @click="onClickConnect"
+    >
+      Connect
+    </CustomBtn>
+    <CustomBtn @click="getAccounts">
+      eth_accounts
+    </CustomBtn>
+    <CustomTextbox title="eth_accounts result">{{accountsResult}}</CustomTextbox>
   </CustomCard>
 </template>
 
@@ -14,6 +21,65 @@ import CustomBtn from '@/components/CustomBtn/CustomBtn.vue';
 
 export default defineComponent({
   name: 'BasicActions',
-  components: { CustomCard, CustomTextbox, CustomBtn }
+  components: { CustomCard, CustomTextbox, CustomBtn },
+  props: {
+    ethereum: Object,
+    default: null
+  },
+  data() {
+      return {
+          accounts: [],
+          accountsResult: 'null',
+          currentUrl: {},
+          btnText: 'Connect',
+          isDisabled: false
+      };
+  },
+  computed: {
+    isMetaMaskConnected() { return this.accounts && this.accounts.length > 0 },
+    forwarderOrigin() { return this.currentUrl.hostname === 'localhost' ? 'http://localhost:9010' : undefined; }
+  },
+  methods: {
+    async onClickConnect() {
+        try {
+            const newAccounts = await this.ethereum.request({
+                method: 'eth_requestAccounts',
+            });
+            this.handleNewAccounts(newAccounts);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async getAccounts() {
+      try {
+        const _accounts = await this.ethereum.request({
+          method: 'eth_accounts',
+        });
+        this.accountsResult =
+          _accounts[0] || 'Not able to get accounts';
+      } catch (err) {
+        console.log(err);
+        this.accountsResult = `Error: ${err.message}`;
+      }
+    },
+    // Move to global helper class?
+    handleNewAccounts(newAccounts) {
+      this.accounts = newAccounts;
+      if (this.isMetaMaskConnected) {
+        console.log('connected');
+        //initializeAccountButtons();
+      }
+      this.updateButtons();
+    },
+    updateButtons() {
+      if (this.isMetaMaskConnected) {
+        this.btnText = 'Connected';
+        this.isDisabled = true;
+      } else {
+        this.btnText = 'Connect';
+        this.isDisabled = false;
+      }
+    }
+  }
 });
 </script>
