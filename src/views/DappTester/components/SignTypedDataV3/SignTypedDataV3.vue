9679a33b75ad9b76552ec4c4b13235b4758cb3e7
radenlmantuano <newcodeondablock@gmail.com>
2022-05-11 11:26:01 -0700

    chore: clean up

--- src/views/DappTester/components/SignTypedDataV3/SignTypedDataV3.vue
+++ src/views/DappTester/components/SignTypedDataV3/SignTypedDataV3.vue
@@ -89,16 +89,16 @@ const signV3 = async () => {
       name: 'Ether Mail',
       version: '1',
       chainId,
-      verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+      verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
     },
     message: {
       from: {
         name: 'Cow',
-        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
       },
       to: {
         name: 'Bob',
-        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
       },
       contents: 'Hello, Bob!'
     }
