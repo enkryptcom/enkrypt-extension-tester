b608d88245d23b2675e043d4b6b3de39f68d9ec8
andrewdahn <andrewahnjr@gmail.com>
2022-05-18 15:59:40 -0700

    devop: push new build

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = defineConfig({
+  publicPath: '/web3-extension-tester/',
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
