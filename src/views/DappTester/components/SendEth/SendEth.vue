a8955570afeb10e5ae217a50c3e83fda9765c75c
andrewdahn <andrewahnjr@gmail.com>
2022-05-23 15:04:07 -0700

    fix: fix send eth

--- src/views/DappTester/components/SendEth/SendEth.vue
+++ src/views/DappTester/components/SendEth/SendEth.vue
@@ -2,7 +2,7 @@
   <CustomCard title="Send Eth">
     <v-text-field
       hide-details
-      v-model="account"
+      v-model="address"
       variant="outlined"
       label="Account (Ethereum address)"
     ></v-text-field>
@@ -24,23 +24,28 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import CustomCard from '@/components/CustomCard/CustomCard.vue';
 import CustomTextbox from '@/components/CustomTextbox/CustomTextbox.vue';
 import CustomBtn from '@/components/CustomBtn/CustomBtn.vue';
 
 const ethereum = window.ethereum;
-
-const account = ref<string>('');
 const txHash = ref<string>('');
 const txHashEIP1559 = ref<string>('');
+
+const props = defineProps({
+  fromAccount: {
+    type: String,
+    default: ''
+  }
+});
 
 const sendTx = async () => {
   const result = await ethereum.request({
     method: 'eth_sendTransaction',
     params: [
       {
-        from: account.value,
+        from: props.fromAccount,
         to: '0x1cC1D4F49E27Ac6b0b7D55A14B8F07179D8aFaf4',
         value: '0x0',
         gasLimit: '0x5028',
@@ -57,7 +62,7 @@ const sendTxEIP1559 = async () => {
     method: 'eth_sendTransaction',
     params: [
       {
-        from: account.value,
+        from: props.fromAccount,
         to: '0x1cC1D4F49E27Ac6b0b7D55A14B8F07179D8aFaf4',
         value: '0x0',
         gasLimit: '0x5028',
@@ -69,16 +74,7 @@ const sendTxEIP1559 = async () => {
   txHashEIP1559.value = result;
 };
 
-const getAccount = async () => {
-  try {
-    const accounts = await ethereum.request({
-      method: 'eth_requestAccounts'
-    });
-    account.value = accounts[0];
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-getAccount();
+const address = computed(() => {
+  return props.fromAccount;
+});
 </script>
